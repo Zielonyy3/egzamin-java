a4a0f7214f4bc3e2c5b9103adab03f87cd5b7ee3
Zielonyy3 <poprostuzielinski@gmail.com>
2021-06-26 15:29:12 +0200

    Zad_3

--- src/com/egzamin/Vehicle.java
+++ src/com/egzamin/Vehicle.java
@@ -1,5 +1,11 @@
 package com.egzamin;
 
 public abstract class Vehicle {
-
+    public String plateNumber;
+    public String vinNumber;
+    public String color;
+    public float price;
+    public float fuelUsage;
+    public int tankFulfillment;
+    public int numberOfKilometers;
 }
